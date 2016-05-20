5b252901da3ce8247f561a82a9e46210ae5762c9
shen_xujing <shen_xujing@ida.gov.sg>
2016-05-20 09:52:23 +0800

    change API endpoint to https://api.beeline.sg

--- beeline-driver/services/BeelineService.js
+++ beeline-driver/services/BeelineService.js
@@ -1,7 +1,7 @@
 // Wrapper for making requests to the beeline api
 export default function($http, TokenService) {
   this.request = function(options) {
-    options.url = "http://staging.beeline.sg" + options.url;
+    options.url = "https://api.beeline.sg" + options.url;
     if (TokenService.token) {
       options.headers = options.headers || {};
       options.headers.authorization = "Bearer " + TokenService.token;
