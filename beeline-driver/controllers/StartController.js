51c88701e2dd6ad6cfeb2be516536e8e357efeed
shen_xujing <shen_xujing@ida.gov.sg>
2016-06-16 16:32:16 +0800

    add passenger list for alight stop

--- beeline-driver/controllers/StartController.js
+++ beeline-driver/controllers/StartController.js
@@ -27,6 +27,8 @@ export default[
     $scope.data ={
       routeId: $stateParams.routeId || undefined,
       tripId: $stateParams.tripId || undefined,
+      board: true,
+      alight: false,
     }
 
     $scope.ping = {
@@ -42,17 +44,7 @@ export default[
     //passenger list per stop is updated automatically
     var reloadPassengersList = function(){
       $timeout.cancel(reloadPassengerTimeout);
-
-      // Get the stop info and count the passengers per stop
-      $scope.$apply(async ()=>{
-        var passengersByStopId = await TripService.getPassengersByStop($scope.data.tripId, true);
-        _.forEach(passengersByStopId, function(value, key) {
-          var stop = $scope.boardStops.find(stop => stop.id === +key);
-          stop.passengerNumber = value.length;
-          stop.passengerList = value;
-        });
-      })
-
+      updatePassengerList();
       //every 1 min reload no. of passenger / stop
       reloadPassengerTimeout = $timeout(reloadPassengersList,60000);
     };
@@ -60,25 +52,35 @@ export default[
     $scope.$on('$ionicView.enter', async () => {
       $scope.data.tripId = $stateParams.tripId;
       console.log("before enter "+$scope.data.tripId);
-
       $scope.trip = await TripService.getTrip($scope.data.tripId, true);
-      $scope.boardStops = $scope.trip.tripStops.filter( stop => stop.canBoard );
+      $scope.stops = $scope.trip.tripStops;
       //stop description e.g. "Yishun 1, Yishun Road 1, RandomPoint, ID 333331"
-      _.forEach($scope.boardStops, function(value, key) {
+      _.forEach($scope.stops, function(value, key) {
         value.stopDescription = value.stop.description + ", " + value.stop.road+ ", " + value.stop.type+ ", ID " + value.stop.label;
       });
+      updatePassengerList();
+      reloadPassengersList();
+      PingService.start($scope.data.tripId);
+    })
+
+    var updatePassengerList = async function(){
       var passengersByStopId = await TripService.getPassengersByStop($scope.data.tripId, true);
       _.forEach(passengersByStopId, function(value, key) {
-        var stop = $scope.boardStops.find(stop => stop.id === +key);
+        var stop = $scope.stops.find(stop => stop.id === +key);
         stop.passengerNumber = value.length;
         stop.passengerList = value;
       });
 
-      $scope.$apply();
-
-      reloadPassengersList();
-      PingService.start($scope.data.tripId);
-    })
+      $scope.boardStops = $scope.trip.tripStops.filter( stop => stop.canBoard );
+      $scope.boardStops = _.sortBy($scope.boardStops, function(item){
+        return item.time;
+      });
+      //stop can be both canBoard and canAlight??
+      $scope.alightStops = $scope.trip.tripStops.filter( stop => stop.canAlight );
+      $scope.alightStops = _.sortBy($scope.alightStops, function(item){
+        return item.time;
+      });
+    }
 
 
     var GPSOffTimeout;
