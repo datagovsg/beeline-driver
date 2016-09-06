7d79b6a7bfd1d53f09e47faecf533864298d6ff6
shen_xujing <shen_xujing@ida.gov.sg>
2016-09-06 10:45:12 +0800

    read backend url from environment variable

--- beeline-driver/services/BeelineService.js
+++ beeline-driver/services/BeelineService.js
@@ -1,7 +1,10 @@
+const env = require('../env.json')
+
 // Wrapper for making requests to the beeline api
 export default function($http, TokenService) {
   this.request = function(options) {
-    options.url = "https://api.beeline.sg" + options.url;
+    // options.url = "https://api.beeline.sg" + options.url;
+    options.url = env.BACKEND_URL + options.url;
     // options.url="https://beeline-server-dev.herokuapp.com"+options.url;
     if (TokenService.token) {
       options.headers = options.headers || {};
