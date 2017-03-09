77a716eb0373dd95e8521339333d2d980a1c88f6
shen_xujing <shen_xujing@ida.gov.sg>
2017-03-09 16:30:38 +0800

    Updates

--- js/bundle.js
+++ js/bundle.js
@@ -153,11 +153,10 @@
 	    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
 	    // for form inputs)
 	    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-	      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-	      cordova.plugins.Keyboard.disableScroll(true);
-	      //keyboard shrinkView
-	      cordova.plugins.Keyboard.shrinkView(true);
+	      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
+	      cordova.plugins.Keyboard.disableScroll(false);
 	    }
+	
 	    if (window.StatusBar) {
 	      // org.apache.cordova.statusbar required
 	      StatusBar.styleLightContent();
