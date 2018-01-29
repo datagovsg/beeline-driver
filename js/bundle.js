f500cb63475909081cbc6b566a67a7c6f600f102
Alwyn Tan <alwyn.tan@data.gov.sg>
2018-01-29 18:02:32 +0800

    Updates

--- js/bundle.js
+++ js/bundle.js
@@ -27291,16 +27291,18 @@
 	});
 
 	exports.default = function ($http, TokenService) {
-	  this.request = function (options) {
-	    // options.url = "https://api.beeline.sg" + options.url;
-	    options.url = env.BACKEND_URL + options.url;
-	    // options.url="https://beeline-server-dev.herokuapp.com"+options.url;
-	    if (TokenService.token) {
-	      options.headers = options.headers || {};
-	      options.headers.Authorization = "Bearer " + TokenService.token;
-	    }
-	    return $http(options);
+	  var sendRequestTo = function sendRequestTo(urlRoot) {
+	    return function (options) {
+	      options.url = urlRoot + options.url;
+	      if (TokenService.token) {
+	        options.headers = options.headers || {};
+	        options.headers.Authorization = "Bearer " + TokenService.token;
+	      }
+	      return $http(options);
+	    };
 	  };
+	  this.request = sendRequestTo(env.BACKEND_URL);
+	  this.tracking = sendRequestTo(env.TRACKING_URL);
 	};
 
 	var env = __webpack_require__(338);
@@ -27311,7 +27313,7 @@
 /* 338 */
 /***/ (function(module, exports) {
 
-	module.exports = {"BACKEND_URL":"https://api.beeline.sg"}
+	module.exports = {"BACKEND_URL":"https://api.beeline.sg","TRACKING_URL":"https://tracking.beeline.sg"}
 
 /***/ }),
 /* 339 */
@@ -37646,9 +37648,9 @@
 	              }
 
 	              _context.next = 15;
-	              return BeelineService.request({
+	              return BeelineService.tracking({
 	                method: "PUT",
-	                url: '/trips/' + self.trip.id + '/setDriver',
+	                url: '/trips/' + self.trip.id + '/roster',
 	                data: optionalData
 	              });
 
