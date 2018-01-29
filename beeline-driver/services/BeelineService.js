faa9e9c6503ddd656cc54f378e8e61237b8a8bd7
Alwyn Tan <alwyn.tan@data.gov.sg>
2018-01-29 10:25:09 +0800

    Send vehicle registration to tracking.beeline.sg
    * Add a function to BeelineService to send requests to beeline-tracking
    * Submit driver/vehicle registration to through `BeelineService.tracking`

--- beeline-driver/services/BeelineService.js
+++ beeline-driver/services/BeelineService.js
@@ -2,14 +2,14 @@ const env = require('../env.json')
 
 // Wrapper for making requests to the beeline api
 export default function($http, TokenService) {
-  this.request = function(options) {
-    // options.url = "https://api.beeline.sg" + options.url;
-    options.url = env.BACKEND_URL + options.url;
-    // options.url="https://beeline-server-dev.herokuapp.com"+options.url;
+  const sendRequestTo = (urlRoot) => function(options) {
+    options.url = urlRoot + options.url;
     if (TokenService.token) {
       options.headers = options.headers || {};
       options.headers.Authorization = "Bearer " + TokenService.token;
     }
     return $http(options);
-  };
+  }
+  this.request = sendRequestTo(env.BACKEND_URL)
+  this.tracking = sendRequestTo(env.TRACKING_URL)
 }
